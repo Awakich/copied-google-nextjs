5136386c4717fcab1413301f7a4f77378f770030
Artem <artem.bqpower@gmail.com>
2022-10-30 15:22:08 +0300

    setup project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Copied Google</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+
+    </div>
+  )
+}
+
+export default Home
